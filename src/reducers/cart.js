0a521bfc967d655531869e4e2d02d151eda17bac
产品研发中心-农经权产品组-胡思路 <hu.silu@cpt.com>
2020-01-16 14:13:35 +0800

    fix: 修改master

--- src/reducers/cart.js
+++ src/reducers/cart.js
@@ -1,31 +1,36 @@
-import actionType from '../actions/actionType'
+// 
+import acitonType from '../actions/actionType'
+// 初始化都是在reducer里面初始化
 const initState = [{
   id: 1,
   title: 'Apple',
-  price: 8888.6,
-  mount: 10
+  price: 999,
+  amount: 10
 },
 {
   id: 2,
   title: 'Orange',
-  price: 4444.6,
-  mount: 10
+  price: 100,
+  amount: 5
 }]
+
+// 创建购物车的reducer， reducer的固定写法是两个参数，第一个是state并有一个初始值，第二个是action
 export default (state = initState, action) => {
+  // 根据不同的action.type 做不同的处理 每次返回一个新的state 返回的类型要一样
   switch (action.type) {
-    case actionType.CART_AMOUNT_INCREMENT:
+    case acitonType.CART_AMOUNT_INCREMENT:
       return state.map(item => {
         if (item.id === action.payload.id) {
-          item.mount += 1;
+          item.amount += 1
         }
-        return item;
+        return item
       })
-    case actionType.CART_AMOUNT_DECREMENT:
+    case acitonType.CART_AMOUNT_DECREMENT:
       return state.map(item => {
         if (item.id === action.payload.id) {
-          item.mount -= 1;
+          item.amount -= 1
         }
-        return item;
+        return item
       })
     default:
       return state
--- src/store.js
+++ src/store.js
@@ -1,7 +1,8 @@
-import { createStore, applyMiddleware } from 'redux'
+// createStore是redux提供的一个 用于创建store的一个方法
+import { createStore } from 'redux';
 
-import thunk from 'redux-thunk'
-
+// 引入合并后的reducer
 import rootReducer from './reducers'
 
-export default createStore(rootReducer, applyMiddleware(thunk))
+// createStore的第一个参数 必须是一个reducer 如果是多个 必须在reducers目录下先使用combineReducers合并之后再导出
+export default createStore(rootReducer)
