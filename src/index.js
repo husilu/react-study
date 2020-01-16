7240cfef6b7c048e7084bf69dca863002df0e4c9
产品研发中心-农经权产品组-胡思路 <hu.silu@cpt.com>
2020-01-16 13:52:25 +0800

    fix: react-redux

--- src/index.js
+++ src/index.js
@@ -1,41 +1,23 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
 
-import { render } from 'react-dom'
+// Provider是react-redux 提供的一个组件
+import { Provider } from 'react-redux';
 
-import App from './App'
+import store from './store';
 
-import AppRedux from './AppRedux'
 
-import * as services from './services'
+ReactDOM.render(
+  // 一般就直接把这个组件放在应用程序的最顶层，这个组件必须有一个store属性，这个store属性的值就是咱们创建的那个store
+  // 只要在最外层包裹了这个Provider，那么所有后台组件都可以使用Redux，connect做链接
+<Provider store={store}>
+  <App/>
+</Provider>, 
+document.getElementById('root'));
 
-import { Provider } from 'react-redux'
-
-import store from './store'
-
-const Counter = () => {
-  useEffect(() => {
-    console.log('更新了！')
-    document.title = `当前数量为${count}`
-  })
-  // console.log()
-  const [count, setCount] = useState(20)
-  return (
-    < div >
-      <p>当前的数量为{count}</p>
-      <button onClick={() => { setCount(count - 1) }}>-</button>
-      <span>{count}</span>
-      <button onClick={() => { setCount(count + 1) }}>+</button>
-    </div >
-  )
-}
-
-React.Component.prototype.http = services
-
-render(
-  <Provider store={store}>
-    <App/>
-    <AppRedux />
-  </Provider>
-  ,
-  document.querySelector('#root')
-)
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+// serviceWorker.unregister();
